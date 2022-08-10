576d58edeb9189ed5efcae9eeddb94c56e11bd46
Siddhi Golatkar <68729177+SiddhiGolatkar@users.noreply.github.com>
2022-08-10 23:32:43 +0530

    Add files via upload

--- Youtube_Audio_video_downloader.py
+++ Youtube_Audio_video_downloader.py
@@ -9,7 +9,7 @@ def main():
     path = st.text_input('Enter URL of youtube video')
     option = st.selectbox('select type of download', ('audio', 'video'))
     # matches = ['audio', 'video'] 
-    if st.button("download", data=path): 
+    if st.button("download"):  
         video_object =  YouTube(path)
         st.write("Title of Video: " + str(video_object.title))
         st.write("Number of Views: " + str(video_object.views))
