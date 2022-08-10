18365c713b9222afc7ad70d52d46e5345cc7d649
Siddhi Golatkar <68729177+SiddhiGolatkar@users.noreply.github.com>
2022-08-10 14:27:40 +0530

    Add files via upload

--- pytube_streamlit.py
+++ pytube_streamlit.py
@@ -1,25 +1,37 @@
 import streamlit as st
 import pandas as pd
 from pytube import YouTube 
+import youtube_dl
 import base64
 from io import BytesIO
 
 def main():
     path = st.text_input('Enter URL of youtube video')
     option = st.selectbox('select type of download', ('audio', 'video'))
-    matches = ['audio', 'video'] 
+    # matches = ['audio', 'video'] 
     if st.button("download"): 
         video_object =  YouTube(path)
         st.write("Title of Video: " + str(video_object.title))
         st.write("Number of Views: " + str(video_object.views))
         if option=='audio':
-            video_object.streams.get_audio_only().download() 	
+            video_info = youtube_dl.YoutubeDL().extract_info(url = path, download=False)
+            filename = f"{video_info['title']}.mp3"
+            options={
+                    'format':'bestaudio/best',
+                    'keepvideo':False,
+                    'outtmpl':filename,
+                    }
+
+            with youtube_dl.YoutubeDL(options) as ydl:
+                ydl.download([video_info['webpage_url']])
+
+            # video_object.streams.get_audio_only().download() 	
             st.write("Audio downloaded")	
             #base64.b64encode("if file is too large").decode()	
         elif option=='video':
             video_object.streams.get_highest_resolution().download()
             st.write("Video downloaded")
-    
+
     if st.button("view"): 
         st.video(path) 
 
